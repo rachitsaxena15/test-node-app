f6769c3b240ccdb0fa7aa24b92245b4e934d925b
Rachit Saxena <rachitsaxena15@gmail.com>
2019-08-02 00:19:53 -0700

    fixing port

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express()
-const PORT = app.eventNames.PORT || 5000;
+const PORT = process.env.PORT || 5000;
 app.get('/', (req, res) => {
     res.send({hi: 'there'});
 });
